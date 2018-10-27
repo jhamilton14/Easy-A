951ceb8b001d9f3289442614fede8aa3e4add8e6
jhamilton14 <jshamilton1414@gmail.com>
2018-10-27 11:15:15 -0400

    Initial Commit

---
+++ Easy A/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Easy A
+//
+//  Created by James Hamilton on 10/27/18.
+//  Copyright © 2018 James Hamilton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
